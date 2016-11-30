e97167072f0dc0c661a9067d3071f3bbc0f50911
Im Hana <ssjk0802@gmail.com>
2016-11-30 22:37:13 +0900

    Implement semi-proper login
    TODO : Login to accounts other than admin

--- login.cpp
+++ login.cpp
@@ -30,8 +30,14 @@ void make_new_user_account(bool admin = false) {
 	}
 
 	fstream pass_dat;
-	pass_dat.open("pass.dat", ios::in | ios::out | ios::binary | ios::trunc);
+	pass_dat.open("pass.dat", ios::in | ios::out | ios::app);
 	
+	if (!pass_dat.is_open()) {
+		// pass.dat does not exist
+		pass_dat.close();
+		pass_dat.open("pass.dat", ios::in | ios::out | ios::trunc | ios::app);
+	}
+
 	/*
 	 * Store username without encryption
 	 * Store password with Caesar's cipher
@@ -42,29 +48,52 @@ void make_new_user_account(bool admin = false) {
 	pass_dat
 		<< username << ' '
 		<< caesar_cipher(password) << ' '
-		<< (admin ? "" : caesar_cipher(to_string(calc_crc32(user_email.c_str())))) << '\n';
+		<< (admin ? "null" : caesar_cipher(to_string(calc_crc32(user_email.c_str())))) << '\n';
 }
 
 void login() {
 	fstream pass_dat;
-	pass_dat.open("pass.dat", ios::in | ios::out | ios::binary);
+	pass_dat.open("pass.dat", ios::in | ios::out);
 
 	if (!pass_dat.is_open()) {
 		// pass.dat does not exist
 		make_new_user_account(true);
 	}
 
-	pass_dat.open("pass.dat", ios::in | ios::out | ios::binary);
-
 	string username;
 	string password;
-	
+	string read_username;
+	string read_password;
+	string read_email; // unused
+
 	cout << "Username : ";
 	getline(cin, username);
 
 	cout << "Password : ";
 	getline(cin, password);
 
-	// debug
-	cout << username << " " << password << endl;
+	// Close and reopen
+	pass_dat.close();
+	pass_dat.open("pass.dat", ios::in | ios::out);
+
+	bool match = false;
+	while (!pass_dat.eof()) {
+		pass_dat >> read_username >> read_password >> read_email;
+		if (username == read_username) {
+			match = true;
+			break;
+		}
+	}
+
+	if (!match) {
+		cerr << "No such username!" << endl;
+		return;
+	}
+
+	if (password != caesar_cipher(read_password, false)) {
+		cout << "Wrong password!" << endl;
+		return;
+	}
+
+	cout << "Login successful!" << endl;
 }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,52 @@
 extern void login();
+extern void make_new_user_account(bool admin = false);
+#include <iostream>
+#include <fstream>
+using namespace std;
+
 
 int main() {
-	login();
+	fstream pass_dat;
+	pass_dat.open("pass.dat", ios::in | ios::out);
+
+	if (!pass_dat.is_open()) {
+		// pass.dat does not exist
+		make_new_user_account(true);
+	}
+
+	int index;
+
+	while(1) {
+		cout << "1. Login" << endl
+			<< "2. New account" << endl
+			<< "3. Find password" << endl
+			<< "4. Quit" << endl;
+
+		cout << "What do you want to do? : ";
+		cin >> index;
+
+		// Clear the buffer for next getline()
+		cin.ignore();
+
+		switch (index)
+		{
+		case 1:
+			login();
+			break;
+		case 2:
+			make_new_user_account();
+			break;
+		/*case 3:
+			find_password();
+			break;*/
+		case 4:
+			return 0;
+		default:
+			cout << "Retry please" << endl;
+			break;
+		}
+	}
+
 
 	return 0;
 }
