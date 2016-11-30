5d34b930b85c942d57ff22e13a703f54d85f7002
Im Hana <ssjk0802@gmail.com>
2016-11-30 20:01:45 +0900

    login: generalize user account creation and store user credentials

--- login.cpp
+++ login.cpp
@@ -4,17 +4,45 @@
 #include <cstring>
 using namespace std;
 
-// TODO : Generalize this function with other user account create function
-void make_new_admin_account() {
-	string admin_pw;
+extern unsigned int calc_crc32(const char* contents);
+extern string caesar_cipher(string val, bool encrypt = true);
 
-	cout << "Enter admin password : ";
-	getline(cin, admin_pw);
+void make_new_user_account(bool admin = false) {
+	string username;
+	string password;
+	string user_email;
+
+	if (admin) {
+		username = "admin";
+	} else {
+		cout << "Enter user name : ";
+		getline(cin, username);
+	}
+
+	cout << "Enter " << username << "'s password : ";
+	getline(cin, password);
+
+	// Admin's email might be publicly available,
+	// do not allow password restoration if admin.
+	if (!admin) {
+		cout << "Enter " << username << "'s Email : ";
+		getline(cin, user_email);
+	}
 
 	fstream pass_dat;
 	pass_dat.open("pass.dat", ios::in | ios::out | ios::binary | ios::trunc);
-
-	pass_dat << "admin " << admin_pw;
+	
+	/*
+	 * Store username without encryption
+	 * Store password with Caesar's cipher
+	 * Store email with Caesar's cipher on top of crc32 hash
+	 *
+	 * Seperator : space
+	 */
+	pass_dat
+		<< username << ' '
+		<< caesar_cipher(password) << ' '
+		<< (admin ? "" : caesar_cipher(to_string(calc_crc32(user_email.c_str())))) << '\n';
 }
 
 void login() {
@@ -23,7 +51,7 @@ void login() {
 
 	if (!pass_dat.is_open()) {
 		// pass.dat does not exist
-		make_new_admin_account();
+		make_new_user_account(true);
 	}
 
 	pass_dat.open("pass.dat", ios::in | ios::out | ios::binary);
---
+++ utils.cpp
@@ -0,0 +1,12 @@
+#include <string>
+using namespace std;
+
+unsigned int calc_crc32(const char* contents) {
+	// TODO : implement
+	return 0;
+}
+
+string caesar_cipher(string val, bool encrypt = true) {
+	// TODO : implement
+	return "";
+}
