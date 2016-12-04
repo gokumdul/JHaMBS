976207ed55069fe5c6a46fdd40169714adf20057
Park Ju Hyung <qkrwngud825@gmail.com>
2016-12-04 20:59:07 +0900

    movie: fix indent style
    This project uses '\t' for indent

--- movie.cpp
+++ movie.cpp
@@ -10,91 +10,91 @@
 using namespace std;
 
 movie::movie(string dat_file) {
-    ifstream movie_dat;
-    movie_dat.open(dat_file);
+	ifstream movie_dat;
+	movie_dat.open(dat_file);
 
-    // First line : parse title
-    getline(movie_dat, title);
-    title = title.substr(8); // 8 : "Title : "
+	// First line : parse title
+	getline(movie_dat, title);
+	title = title.substr(8); // 8 : "Title : "
 
-    // Second line : parse runtime
-    runtime = 0; // Initialize member
-    string runtime_string;
-    getline(movie_dat, runtime_string);
-    runtime_string = runtime_string.substr(11); // 11 : "Run time : "
-    istringstream runtime_iss(runtime_string);
-    string runtime_word; // Temporarily used for parsing runtime string
-    while (runtime_iss >> runtime_word) {
-        if (string_ends_with(runtime_word, "h")) {
-            runtime += stoi(runtime_word.substr(0, runtime_word.size() - 1)) * 60;
-        } else if (string_ends_with(runtime_word, "min")) {
-            runtime += stoi(runtime_word.substr(0, runtime_word.size() - 3));
-        }
-    }
+	// Second line : parse runtime
+	runtime = 0; // Initialize member
+	string runtime_string;
+	getline(movie_dat, runtime_string);
+	runtime_string = runtime_string.substr(11); // 11 : "Run time : "
+	istringstream runtime_iss(runtime_string);
+	string runtime_word; // Temporarily used for parsing runtime string
+	while (runtime_iss >> runtime_word) {
+		if (string_ends_with(runtime_word, "h")) {
+			runtime += stoi(runtime_word.substr(0, runtime_word.size() - 1)) * 60;
+		} else if (string_ends_with(runtime_word, "min")) {
+			runtime += stoi(runtime_word.substr(0, runtime_word.size() - 3));
+		}
+	}
 
-    // Third line : parse screen types
-    IMAX = false; // Initialize member
-    threeD = false; // Initialize member
-    string screens_string;
-    getline(movie_dat, screens_string);
-    screens_string = screens_string.substr(10); // 10 : "Screens : "
-    // Replace ',' with a whitespace for 'screens_iss >> screens_word' to work properly
-    replace(screens_string.begin(), screens_string.end(), ',', ' ');
-    istringstream screens_iss(screens_string);
-    string screens_word; // Temporarily used for parsing screens string
-    while (screens_iss >> screens_word) {
-        if (screens_word == "IMAX")
-            IMAX = true;
-        if (screens_word == "3D")
-            threeD = true;
-    }
+	// Third line : parse screen types
+	IMAX = false; // Initialize member
+	threeD = false; // Initialize member
+	string screens_string;
+	getline(movie_dat, screens_string);
+	screens_string = screens_string.substr(10); // 10 : "Screens : "
+	// Replace ',' with a whitespace for 'screens_iss >> screens_word' to work properly
+	replace(screens_string.begin(), screens_string.end(), ',', ' ');
+	istringstream screens_iss(screens_string);
+	string screens_word; // Temporarily used for parsing screens string
+	while (screens_iss >> screens_word) {
+		if (screens_word == "IMAX")
+			IMAX = true;
+		if (screens_word == "3D")
+			threeD = true;
+	}
 
-    // Clean up
-    movie_dat.close();
+	// Clean up
+	movie_dat.close();
 }
 
 string movie::get_title() const {
-    return title;
+	return title;
 }
 int movie::get_runtime() const {
-    return runtime;
+	return runtime;
 }
 bool movie::is_IMAX() const {
-    return IMAX;
+	return IMAX;
 }
 bool movie::is_3D() const {
-    return threeD;
+	return threeD;
 }
 
 void book_movie() {
-    vector<string> movie_dats = listdir("movies", "movie-", ".dat");
-    vector<string> movie_titles;
-    vector<string>::iterator it_string;
+	vector<string> movie_dats = listdir("movies", "movie-", ".dat");
+	vector<string> movie_titles;
+	vector<string>::iterator it_string;
 
-    vector<movie> movie_vec;
-    vector<movie>::iterator it_movie;
+	vector<movie> movie_vec;
+	vector<movie>::iterator it_movie;
 
-    for (it_string = movie_dats.begin(); it_string != movie_dats.end(); it_string++)
-        movie_vec.push_back(movie("movies/" + *it_string));
+	for (it_string = movie_dats.begin(); it_string != movie_dats.end(); it_string++)
+		movie_vec.push_back(movie("movies/" + *it_string));
 
-    string* movies_array = new string[movie_vec.size()];
-    int i = 0;
-    for (it_movie = movie_vec.begin(); it_movie != movie_vec.end(); it_movie++) {
-        movies_array[i++] = it_movie->get_title();
-    }
+	string* movies_array = new string[movie_vec.size()];
+	int i = 0;
+	for (it_movie = movie_vec.begin(); it_movie != movie_vec.end(); it_movie++) {
+		movies_array[i++] = it_movie->get_title();
+	}
 
-    int user_choice = print_menu("Movies now playing", movies_array, i);
-    ifstream movie_dat;
-    movie_dat.open("movies/" + *((it_string = movie_dats.begin()) + user_choice - 1));
+	int user_choice = print_menu("Movies now playing", movies_array, i);
+	ifstream movie_dat;
+	movie_dat.open("movies/" + *((it_string = movie_dats.begin()) + user_choice - 1));
 
-    int lines = count(istreambuf_iterator<char>(movie_dat), istreambuf_iterator<char>(), '\n');
-    movie_dat.seekg(0, ios::beg);
+	int lines = count(istreambuf_iterator<char>(movie_dat), istreambuf_iterator<char>(), '\n');
+	movie_dat.seekg(0, ios::beg);
 
-    string* strings = new string[lines];
-    for (int i = 0; getline(movie_dat >> ws, strings[i]); i++); // ws : absorb whitespace
+	string* strings = new string[lines];
+	for (int i = 0; getline(movie_dat >> ws, strings[i]); i++); // ws : absorb whitespace
 
-    print_menu("Movie details", strings, lines, false);
+	print_menu("Movie details", strings, lines, false);
 
-    // TODO : Actually book a movie
-    exit(0);
+	// TODO : Actually book a movie
+	exit(0);
 }
--- movie.hpp
+++ movie.hpp
@@ -3,15 +3,15 @@ using namespace std;
 
 class movie {
 private:
-    string title;
-    int runtime; // In minutes
-    bool IMAX;
-    bool threeD; // Variable cannot start with a number :(
+	string title;
+	int runtime; // In minutes
+	bool IMAX;
+	bool threeD; // Variable cannot start with a number :(
 
 public:
-    movie(string dat_file);
-    string get_title() const;
-    int get_runtime() const;
-    bool is_IMAX() const;
-    bool is_3D() const;
+	movie(string dat_file);
+	string get_title() const;
+	int get_runtime() const;
+	bool is_IMAX() const;
+	bool is_3D() const;
 };
